c28a06da80534b1f761abf9168af2746eab6e19c
CoderTaoMin <weryoung@foxmail.com>
2018-11-22 18:04:29 +0800

    Revert "实现淘宝首页滑动广告效果"
    This reverts commit c224283ed1849ade3b8f0e6252325f68fbdb1c6a.

---
+++ app/src/main/java/com/qfxl/marquee/AdvEntity.java
@@ -0,0 +1,50 @@
+package com.qfxl.marquee;
+
+/**
+ * <pre>
+ *     author : qfxl
+ *     e-mail : xuyonghong0822@gmail.com
+ *     time   : 2018/11/21
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+public class AdvEntity {
+
+    private String price;
+    private String promotionalPrice;
+    private String content;
+
+    public AdvEntity() {
+    }
+
+    public AdvEntity(String price, String promotionalPrice, String content) {
+        this.price = price;
+        this.promotionalPrice = promotionalPrice;
+        this.content = content;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getPromotionalPrice() {
+        return promotionalPrice;
+    }
+
+    public void setPromotionalPrice(String promotionalPrice) {
+        this.promotionalPrice = promotionalPrice;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
